be5e354cc96ee75665fa99f16b81b6161ae494f1
Clark Kampfe <clark@ckfr.local>
2018-09-08 00:47:09 -0500

    update benchmarks for with/without AVET

--- bench/insert.exs
+++ bench/insert.exs
@@ -1,12 +1,32 @@
 alias Elog.Db
 
-simple =
-  Stream.cycle([%{name: "Bill"}, %{name: "Bob"}, %{name: "Grendel"}])
+simple = Stream.cycle([%{name: "Bill"}, %{name: "Bob"}, %{name: "Grendel"}])
 
 complex =
-  Stream.cycle([%{name: "Bill", age: 23, height: 723, interests: ["Squash", "Candy", "great old movies"], marital_status: "married to the game"},
-                %{name: "Sandy", immigration_status: "doesn't matter", other_crap: %{a: 1, b: 2, c: 3}, height: 4, favorite_band: "Wu tang"},
-               %{favorite_band: "Juve", favorite_soccer: "Juve", favorite_color: "red", favorite_candy: "reeses", name: "Eleanor", age: 92}])
+  Stream.cycle([
+    %{
+      name: "Bill",
+      age: 23,
+      height: 723,
+      interests: ["Squash", "Candy", "great old movies"],
+      marital_status: "married to the game"
+    },
+    %{
+      name: "Sandy",
+      immigration_status: "doesn't matter",
+      other_crap: %{a: 1, b: 2, c: 3},
+      height: 4,
+      favorite_band: "Wu tang"
+    },
+    %{
+      favorite_band: "Juve",
+      favorite_soccer: "Juve",
+      favorite_color: "red",
+      favorite_candy: "reeses",
+      name: "Eleanor",
+      age: 92
+    }
+  ])
 
 tiny = 10
 small = 1000
@@ -21,23 +41,48 @@ small_complex = Enum.take(complex, small)
 medium_complex = Enum.take(complex, medium)
 large_complex = Enum.take(complex, large)
 
-########3
-
 example_small_db = Db.new(small_simple)
 example_medium_db = Db.new(medium_simple)
 example_large_db = Db.new(large_simple)
+example_small_db_no_avet = Db.new(small_simple, %{indexes: [:eavt, :aevt]})
+example_medium_db_no_avet = Db.new(medium_simple, %{indexes: [:eavt, :aevt]})
+example_large_db_no_avet = Db.new(large_simple, %{indexes: [:eavt, :aevt]})
 
-tiny_insert = Enum.take(simple, 10)
+tiny_insert = Enum.take(simple, tiny)
 
-Benchee.run(%{
-  "new small simple"    => fn -> Db.new(small_simple) end,
-  "new medium simple" => fn -> Db.new(medium_simple) end,
-  # "new large simple" => fn -> Db.new(large_simple) end,
-  "new small complex" => fn -> Db.new(small_complex) end,
-  "new medium complex" => fn -> Db.new(medium_complex) end,
-   #"new large complex" => fn -> Db.new(large_complex) end,
-
-  "insert tiny - small simple" => fn -> Db.transact(example_small_db, tiny_insert) end,
-  "insert tiny - medium simple" => fn -> Db.transact(example_medium_db, tiny_insert) end,
-  "insert tiny - large simple" => fn -> Db.transact(example_large_db, tiny_insert) end,
-}, time: 10, memory_time: 2)
+Benchee.run(
+  %{
+    "new small simple" => fn -> Db.new(small_simple) end,
+    "new medium simple" => fn -> Db.new(medium_simple) end,
+    # "new large simple" => fn -> Db.new(large_simple) end,
+    "new small complex" => fn -> Db.new(small_complex) end,
+    "new medium complex" => fn -> Db.new(medium_complex) end,
+    # "new large complex" => fn -> Db.new(large_complex) end,
+    "new small simple no avet" => fn ->
+      Db.new(small_simple, %{indexes: [:eavt, :aevt]})
+    end,
+    "new medium simple no avet" => fn ->
+      Db.new(medium_simple, %{indexes: [:eavt, :aevt]})
+    end,
+    "transact tiny - small simple" => fn ->
+      Db.transact(example_small_db, tiny_insert)
+    end,
+    "transact tiny - medium simple" => fn ->
+      Db.transact(example_medium_db, tiny_insert)
+    end,
+    "transact tiny - large simple" => fn ->
+      Db.transact(example_large_db, tiny_insert)
+    end,
+    "transact tiny - small simple no avet" => fn ->
+      Db.transact(example_small_db_no_avet, tiny_insert)
+    end,
+    "transact tiny - medium simple no avet" => fn ->
+      Db.transact(example_medium_db_no_avet, tiny_insert)
+    end,
+    "transact tiny - large simple no avet" => fn ->
+      Db.transact(example_large_db_no_avet, tiny_insert)
+    end
+  },
+  time: 5,
+  memory_time: 2
+)
--- bench/query.exs
+++ bench/query.exs
@@ -1,25 +1,33 @@
 alias Elog.Db
 import Elog.Syntax
 
-simple =
-  Stream.cycle([%{name: "Bill"}, %{name: "Bob"}, %{name: "Grendel"}])
+simple = Stream.cycle([%{name: "Bill"}, %{name: "Bob"}, %{name: "Grendel"}])
 
 complex =
-  Stream.cycle([%{name: "Bill",
-                  age: 23,
-                  height: 723,
-                  interests: ["Squash", "Candy", "great old movies"],
-                  marital_status: "married to the game"},
-                %{name: "Sandy",
-                  immigration_status: "doesn't matter",
-                  other_crap: %{a: 1, b: 2, c: 3},
-                  height: 4,
-                  favorite_band: "Wu tang"},
-                %{favorite_band: "Juve",
-                  favorite_soccer: "Juve",
-                  favorite_color: "red",
-                  favorite_candy: "reeses",
-                  name: "Eleanor", age: 92}])
+  Stream.cycle([
+    %{
+      name: "Bill",
+      age: 23,
+      height: 723,
+      interests: ["Squash", "Candy", "great old movies"],
+      marital_status: "married to the game"
+    },
+    %{
+      name: "Sandy",
+      immigration_status: "doesn't matter",
+      other_crap: %{a: 1, b: 2, c: 3},
+      height: 4,
+      favorite_band: "Wu tang"
+    },
+    %{
+      favorite_band: "Juve",
+      favorite_soccer: "Juve",
+      favorite_color: "red",
+      favorite_candy: "reeses",
+      name: "Eleanor",
+      age: 92
+    }
+  ])
 
 small = 100
 medium = 1_000
@@ -33,10 +41,22 @@ small_complex = Enum.take(complex, small)
 medium_complex = Enum.take(complex, medium)
 large_complex = Enum.take(complex, large)
 
-simple_variable_find = %{find: [~q(e), ~q(name)], where: [[~q(e), :name, ~q(name)]]}
+simple_variable_find = %{
+  find: [~q(e), ~q(name)],
+  where: [[~q(e), :name, ~q(name)]]
+}
+
 simple_literal_find = %{find: [~q(e)], where: [[~q(e), :name, "Bob"]]}
-simple_literal_find_join_literal_first = %{find: [~q(e), ~q(name)], where: [[~q(e), :name, "Bob"], [~q(e), :name, ~q(name)]]}
-simple_literal_find_join_join_first = %{find: [~q(e), ~q(name)], where: [[~q(e), :name, ~q(name)], [~q(e), :name, "Bob"]]}
+
+simple_literal_find_join_literal_first = %{
+  find: [~q(e), ~q(name)],
+  where: [[~q(e), :name, "Bob"], [~q(e), :name, ~q(name)]]
+}
+
+simple_literal_find_join_join_first = %{
+  find: [~q(e), ~q(name)],
+  where: [[~q(e), :name, ~q(name)], [~q(e), :name, "Bob"]]
+}
 
 small_simple_db = Db.new(small_simple)
 medium_simple_db = Db.new(medium_simple)
@@ -46,26 +66,51 @@ small_complex_db = Db.new(small_complex)
 medium_complex_db = Db.new(medium_complex)
 large_complex_db = Db.new(large_complex)
 
-Benchee.run(%{
-  "small simple variable find" => fn -> Db.query(small_simple_db, simple_variable_find) end,
-  "medium simple variable find" => fn -> Db.query(medium_simple_db, simple_variable_find) end,
-  "large simple variable find" => fn -> Db.query(large_simple_db, simple_variable_find) end,
-
-  "small simple literal find" => fn -> Db.query(small_simple_db, simple_literal_find) end,
-  "medium simple literal find" => fn -> Db.query(medium_simple_db, simple_literal_find) end,
-  "large simple literal find" => fn -> Db.query(large_simple_db, simple_literal_find) end,
-
-  "small simple literal find join literal first" => fn -> Db.query(small_simple_db, simple_literal_find_join_literal_first) end,
-  "medium simple literal find join literal first" => fn -> Db.query(medium_simple_db, simple_literal_find_join_literal_first) end,
-
-  "large simple literal find join literal first" => fn -> Db.query(large_simple_db, simple_literal_find_join_literal_first) end,
-
-  "small simple literal find join, join first" => fn -> Db.query(small_simple_db, simple_literal_find_join_join_first) end,
-  "medium simple literal find join, join first" => fn -> Db.query(medium_simple_db, simple_literal_find_join_join_first) end,
-
-  "large simple literal find join join first" => fn -> Db.query(large_simple_db, simple_literal_find_join_join_first) end,
-
-  "small complex find" => fn -> Db.query(small_complex_db, simple_variable_find) end,
-  "medium complex find" => fn -> Db.query(medium_complex_db, simple_variable_find) end,
-
-}, time: 10, memory_time: 2)
+Benchee.run(
+  %{
+    "small simple variable find" => fn ->
+      Db.query(small_simple_db, simple_variable_find)
+    end,
+    "medium simple variable find" => fn ->
+      Db.query(medium_simple_db, simple_variable_find)
+    end,
+    "large simple variable find" => fn ->
+      Db.query(large_simple_db, simple_variable_find)
+    end,
+    "small simple literal find" => fn ->
+      Db.query(small_simple_db, simple_literal_find)
+    end,
+    "medium simple literal find" => fn ->
+      Db.query(medium_simple_db, simple_literal_find)
+    end,
+    "large simple literal find" => fn ->
+      Db.query(large_simple_db, simple_literal_find)
+    end,
+    "small simple literal find join literal first" => fn ->
+      Db.query(small_simple_db, simple_literal_find_join_literal_first)
+    end,
+    "medium simple literal find join literal first" => fn ->
+      Db.query(medium_simple_db, simple_literal_find_join_literal_first)
+    end,
+    "large simple literal find join literal first" => fn ->
+      Db.query(large_simple_db, simple_literal_find_join_literal_first)
+    end,
+    "small simple literal find join, join first" => fn ->
+      Db.query(small_simple_db, simple_literal_find_join_join_first)
+    end,
+    "medium simple literal find join, join first" => fn ->
+      Db.query(medium_simple_db, simple_literal_find_join_join_first)
+    end,
+    "large simple literal find join join first" => fn ->
+      Db.query(large_simple_db, simple_literal_find_join_join_first)
+    end,
+    "small complex find" => fn ->
+      Db.query(small_complex_db, simple_variable_find)
+    end,
+    "medium complex find" => fn ->
+      Db.query(medium_complex_db, simple_variable_find)
+    end
+  },
+  time: 5,
+  memory_time: 2
+)
